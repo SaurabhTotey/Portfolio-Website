16370971df520fda31459bbd1d412c42cfccd5c4
Saurabh Totey <SaurabhTotey@gmail.com>
2017-12-22 14:02:11 -0700

    Migrated project to use Spring Boot MVC and Kotlin

---
+++ src/main/kotlin/com/saurabhtotey/portfoliowebsite/controller/PageController.kt
@@ -0,0 +1,15 @@
+package com.saurabhtotey.portfoliowebsite.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+open class PageController {
+
+    @RequestMapping("/index")
+    fun homePage(model: Model): String {
+        return "index"
+    }
+
+}
