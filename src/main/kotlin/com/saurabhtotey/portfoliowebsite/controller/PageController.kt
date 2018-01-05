8355b99c4ef322f182ecf06e9f86a282f363ea5f
Saurabh Totey <SaurabhTotey@gmail.com>
2018-01-05 11:27:00 -0700

    Made navbar fragment parametrized and more flexible

--- src/main/kotlin/com/saurabhtotey/portfoliowebsite/controller/PageController.kt
+++ src/main/kotlin/com/saurabhtotey/portfoliowebsite/controller/PageController.kt
@@ -7,9 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping
 @Controller
 open class PageController {
 
+    /**
+     * Serves the Home page as the first page of the website; is the index
+     */
     @RequestMapping("/")
     fun homePage(): String {
-        return "index"
+        return "Home"
+    }
+
+    /**
+     * When the user tries to go the website/Home, they will get redirected to the home page at just /
+     */
+    @RequestMapping("/Home")
+    fun redirectToHomePage(): String {
+        return "redirect:"
     }
 
 }
