81691779e345d6eab810ef827164ff08a3883901
Saurabh Totey <SaurabhTotey@gmail.com>
2018-04-03 20:44:24 -0600

    Updated dependencies and collapsed ErrorController into PageController

--- src/main/kotlin/com/saurabhtotey/portfoliowebsite/controller/PageController.kt
+++ src/main/kotlin/com/saurabhtotey/portfoliowebsite/controller/PageController.kt
@@ -1,5 +1,6 @@
 package com.saurabhtotey.portfoliowebsite.controller
 
+import org.springframework.boot.web.servlet.error.ErrorController
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
  * The class that serves all of the pages and handles filling out and sending web pages
  */
 @Controller
-open class PageController {
+open class PageController: ErrorController {
 
     /**
      * Serves the Home page as the first page of the website; is the index
@@ -18,11 +19,34 @@ open class PageController {
     }
 
     /**
-     * When the user tries to go the website/Home, they will get redirected to the home page at just /
+     * Serves the Error page if asked for
+     */
+    @RequestMapping("/Error")
+    fun errorPage(): String {
+        return "Error"
+    }
+
+    /**
+     * Gets the path of where this controller will handle errors
+     */
+    override fun getErrorPath(): String {
+        return "/Error"
+    }
+
+    /**
+     * When the user tries to go to /Home, they will get redirected to the home page at just /
      */
     @RequestMapping("/Home")
     fun redirectToHomePage(): String {
         return "redirect:"
+    }
+
+    /**
+     * When the user tries to go to /error, they will get redirected to the error page at /Error
+     */
+    @RequestMapping("/error")
+    fun redirectToErrorPage(): String {
+        return "redirect:/Error"
     }
 
 }
