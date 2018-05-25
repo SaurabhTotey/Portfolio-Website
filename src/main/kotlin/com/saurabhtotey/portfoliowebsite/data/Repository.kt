715ff83cd6a166fd0cce051be8b2c04041ea625a
Saurabh Totey <SaurabhTotey@gmail.com>
2018-05-24 23:49:46 -0600

    Made a footer and updated interest descriptions

--- src/main/kotlin/com/saurabhtotey/portfoliowebsite/data/Repository.kt
+++ src/main/kotlin/com/saurabhtotey/portfoliowebsite/data/Repository.kt
@@ -1,3 +1,20 @@
 package com.saurabhtotey.portfoliowebsite.data
 
-//TODO: GitHub Repository stuff using GitHub API and bestRepositories.json
+import java.util.*
+
+/**
+ * The class that represents a GitHub repository
+ */
+data class Repository(val name: String, val url: String, val homepage: String, val languages: Array<String>, val img: String = "")
+
+/**
+ * A singleton that represents all of the repository information
+ */
+object RepositoryInformation {
+
+    //The last time the repository information was updated
+    val lastUpdated = Date()
+    lateinit var allRepositories: Array<Repository>
+    lateinit var bestRepositories: Array<Repository>
+    
+}
