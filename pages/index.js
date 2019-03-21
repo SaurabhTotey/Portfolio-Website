07dade9ebdfb6aa22b8134428fa21d281ec81971
Saurabh Totey <SaurabhTotey@gmail.com>
2019-03-21 13:56:19 -0600

    Made a tooltip in index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Tooltip from '@material-ui/core/Tooltip';
+import Fade from "@material-ui/core/Fade";
 import Header from "../components/Header"
 
 export default () => (
 	<div>
 		<Header />
-		<h1>Saurabh Totey</h1>
+		<Tooltip TransitionComponent={Fade} enterDelay={10000} enterTouchDelay={10000} title="Just in case you didn't know whose website you were on.">
+			<h1>Saurabh Totey</h1>
+		</Tooltip>
+		<style jsx>{`
+			text-align: center;
+		`}</style>
 	</div>
 )
