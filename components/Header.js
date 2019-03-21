e2a2f71f08dab280e6d0cc307537747a4c66e002
Saurabh Totey <SaurabhTotey@gmail.com>
2019-03-21 09:16:06 -0600

    Refactored project to be static React website

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+export default () => (
+	<div>
+		<Head>
+			<title>Saurabh Totey</title>
+		</Head>
+		<style jsx global>{`
+			body {
+				background-color: #EEEEEE;
+				color: #454545;
+			}
+
+			a {
+				color: #0077AA;
+			}
+		`}</style>
+	</div>
+)
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "../components/Header"
+
+export default () => (
+	<div>
+		<Header />
+		<h1>Saurabh Totey</h1>
+	</div>
+)
