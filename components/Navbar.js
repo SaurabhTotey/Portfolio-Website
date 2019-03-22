a76aa64faeee626b57327ca62823ccb576f21d3a
Saurabh Totey <SaurabhTotey@gmail.com>
2019-03-22 15:20:38 -0600

    Migrated project to Gatsby

--- components/Navbar.js
+++
@@ -1,7 +0,0 @@
-import Link from "next/link";
-
-export default () => (
-	<div>
-
-	</div>
-);
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default () => (
+	<div>
+		<head>
+			<title>Saurabh Totey</title>
+		</head>
+		<style jsx global>{`
+			body {
+				background-color: #EEEEEE;
+				color: #454545;
+			}
+
+			a {
+				color: #0077AA;
+			}
+		`}</style>
+	</div>
+)
