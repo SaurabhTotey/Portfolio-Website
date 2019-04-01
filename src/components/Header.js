3d1f663c954addb1f0e15a5fb7a100cb8a602d3f
Saurabh Totey <SaurabhTotey@gmail.com>
2019-03-31 21:02:55 -0600

    Removed Bootstrap from project entirely

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,9 @@
 import React from "react";
 import Navbar from "./Navbar";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 export default () => (
 	<div>
-		<OverlayTrigger overlay={<div style={{fontSize: "10px"}}>Just in case you didn't know whose website you were on.</div>} delay={{ show: 10000 }} placement={"bottom"}>
-			<h1 style={{textAlign: "center"}}>Saurabh Totey</h1>
-		</OverlayTrigger>
+		<h1 style={{textAlign: "center"}}>Saurabh Totey</h1>
 		<Navbar />
 	</div>
 )
