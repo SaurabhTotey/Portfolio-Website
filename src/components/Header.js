84a553c816f6eff907f0789a49d05522907edcda
Saurabh Totey <SaurabhTotey@gmail.com>
2019-04-27 15:58:54 -0600

    Updated tooltip and About Me description and made my age get calculated

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default () => (
 	<div>
 		<div id={"title-container"}>
 			<h1 id={"title"}>Saurabh Totey</h1>
-			<span id={"secret-tooltip"}>Just in case you didn't know whose website you were on.</span>
+			<span id={"secret-tooltip"}>Just in case you didn't know whose website you were on. Read about me on the home page if you don't know what biological species I am.</span>
 		</div>
 		<Navbar />
 	</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default () => (
 		<h2>About me</h2>
 		<ResponsiveContainer>
 			<img src={"images/SaurabhToteyCancun.jpg"} alt={"Saurabh Totey"} style={{width: "100%"}} responsiveWidth="25%" />
-			<p responsiveWidth="75%" style={{margin: "10px"}}>Hello! I am Saurabh Totey. I am an 18 year-old male with a strong passion for science and technology. I currently live in Boulder, Colorado with my parents and my younger brother. In my free time, I spend a lot of my time trying to learn. I love learning all sorts of things such as programming, playing the piano, playing the cello, and modelling 3D objects. However, that isn't to say that I don't spend some of my free time doing other activities such as playing videogames or watching television series. I hope one day to make a great change in the world.</p>
+			<p responsiveWidth="75%" style={{margin: "10px"}}>Hello! I am Saurabh Totey. I am a human male that is {new Date(new Date() - new Date(2000, 12, 5)).getFullYear() - 1970} years old. I currently live in Boulder, Colorado with my parents and my younger brother. In my free time, I spend a lot of my time trying to learn. My main passions are science and technology, but more specifically, I love learning all sorts of things such as programming, playing the piano, playing the cello, and modelling 3D objects. However, that isn't to say that I don't spend some of my free time doing other activities such as playing videogames or watching television series. I hope one day to make a great change in the world.</p>
 		</ResponsiveContainer>
 		<br/>
 		<h2>Things I like</h2>
