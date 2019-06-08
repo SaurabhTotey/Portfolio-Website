ee3506477553853b69cd161be232c5544c688e70
Saurabh Totey <SaurabhTotey@gmail.com>
2019-06-08 16:19:19 -0600

    Made ResponsiveContainer manage its styling rather than CSS

--- src/components/ResponsiveContainer.js
+++ src/components/ResponsiveContainer.js
@@ -1,10 +1,12 @@
 import React from "react";
-import "../styles/ResponsiveContainer.css";
 
 /**
  * A class that represents a responsive container
  * Will hold elements horizontally unless screen width is small
  * If screen width is small, components will be held vertically
+ * Requires all children to define their 'responsiveWidth' prop
+ * responsiveWidth determines how much space a child takes up horizontally on large screens
+ * responsiveWidths of all children must add up to 12
  */
 class ResponsiveContainer extends React.Component {
 
@@ -25,13 +27,21 @@ class ResponsiveContainer extends React.Component {
 	}
 
 	render() {
-		return <div className={"responsiveContainer"}>{
+		const containerStyling = { display: "grid", gridTemplateColumns: "100%" };
+		if (!this.state.isVertical) {
+			const columnWidth = `${100 / 12}% `;
+			containerStyling.gridTemplateColumns = "";
+			for (let i = 0; i < 12; i++) {
+				containerStyling.gridTemplateColumns += columnWidth;
+			}
+		}
+		return <div style={containerStyling}>{
 			this.props.children.map( child => {
-				let span = child.props.responsiveWidth;
-				if (this.state.isVertical) {
-					span = 1;
+				const itemStyling = { placeSelf: "stretch", gridArea: "span 1 / span 1" };
+				if (!this.state.isVertical) {
+					itemStyling.gridArea = `span 1 / span ${child.props.responsiveWidth}`;
 				}
-				return <div className={"responsiveContainerItem"} style={{gridArea: `span 1 / span ${span}`}}>{child}</div>
+				return <div style={itemStyling}>{child}</div>
 			})
 		}</div>
 	}
