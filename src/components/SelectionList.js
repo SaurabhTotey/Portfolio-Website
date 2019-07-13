34e02e560abad9ee81048a9b1f684cba2446b5f9
Saurabh Totey <SaurabhTotey@gmail.com>
2019-07-13 10:09:01 -0600

    Made aria roles for SelectionList more accurate

--- src/components/SelectionList.js
+++ src/components/SelectionList.js
@@ -32,9 +32,9 @@ class SelectionList extends React.Component {
 	}
 
 	render() {
-		return <nav role={"navigation"} aria-label={this.props.ariaLabel} aria-controls={this.props.ariaControls}>
+		return <nav role={"menu"} aria-label={this.props.ariaLabel} aria-controls={this.props.ariaControls}>
 			<ul className={"selectionList"}>{
-				this.state.items.map((item, index) => <li className={"selectionListItem"} key={item}><button className={`selectionItem ${index === this.state.selectedItemIndex? "active" : ""}`} onClick={() => this.setState({ ...this.state, selectedItemIndex: index })}>{item}</button></li>)
+				this.state.items.map((item, index) => <li className={"selectionListItem"} key={item} role={"menuitemradio"}><button className={`selectionItem ${index === this.state.selectedItemIndex? "active" : ""}`} onClick={() => this.setState({ ...this.state, selectedItemIndex: index })}>{item}</button></li>)
 			}</ul>
 		</nav>
 	}
