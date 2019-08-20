06465d885bce01fd376478f9395ca96b644a9d0e
SaurabhTotey <SaurabhTotey@gmail.com>
2019-08-20 10:41:58 -0600

    Implemented carousel switching animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -275,16 +275,24 @@ class App extends React.Component {
 				<div responsiveWidth="4" style={{height: "100%", display: "flex", flexDirection: "column", justifyContent: "center"}}>
 					<SelectionList items={carouselInformation.map(information => information.title)} onSelection={category => this.setState({ ...this.state, currentSelectedCategory: category })} ariaLabel={"A list of buttons that make the carousel display my favorite items from the button's named category."} ariaControls={"carouselsContainer categoryDescriptionsContainer"} />
 				</div>
-				<div responsiveWidth="8" style={{margin: "10px"}} id={"carouselsContainer"} aria-live={"polite"}>{
-					carouselInformation.map(information => <div className={this.state.currentSelectedCategory !== information.title ? "hidden" : ""} aria-hidden={this.state.currentSelectedCategory !== information.title} key={information.title}>
-						<Carousel title={information.title}>{
-							information.items.map(item => <div key={item}>
-								<h4>{item.name}</h4>
-								<img src={item.imageUrl} alt={item.name} height={150} style={{maxWidth: "295px"}} />
-								<p>{item.description}</p>
-							</div>)
-						}</Carousel>
-					</div>)
+				<div responsiveWidth="8" style={{margin: "10px", position: "relative", height: "50rem"}} id={"carouselsContainer"} aria-live={"polite"}>{
+					carouselInformation.map(information => {
+						let className = this.state.currentSelectedCategory === information.title ? "" : "hidden";
+						const style = {};
+						if (this.previousSelectedCategory !== "" && [this.previousSelectedCategory, this.state.currentSelectedCategory].includes(information.title)) {
+							className = "animation";
+							style["--animation-name"] = this.state.currentSelectedCategory === information.title ? "fadeIn" : "fadeOut";
+						}
+						return <div className={`carouselContainer ${className}`} style={style} aria-hidden={this.state.currentSelectedCategory !== information.title} key={information.title}>
+							<Carousel title={information.title}>{
+								information.items.map(item => <div key={item}>
+									<h4>{item.name}</h4>
+									<img src={item.imageUrl} alt={item.name} height={150} style={{maxWidth: "295px"}} />
+									<p>{item.description}</p>
+								</div>)
+							}</Carousel>
+						</div>
+					})
 				}</div>
 			</ResponsiveContainer>
 			<div id={"categoryDescriptionsContainer"} aria-live={"polite"} style={{textAlign: "center", minHeight: "12rem", display: "flex", flexDirection: "column", justifyContent: "center", position: "relative"}}>{
