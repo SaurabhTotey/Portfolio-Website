3408d33c2acccd086a3d3e02d1430910a9baa4b8
Saurabh Totey <SaurabhTotey@gmail.com>
2021-01-26 23:20:29 -0700

    Plans for programming page

--- src/pages/Programming.js
+++ src/pages/Programming.js
@@ -3,6 +3,34 @@ import Layout from "../components/Layout";
 
 export default () => (
 	<Layout>
-		<p>Sorry, this page is a work in progress! In the meantime, feel free to check out my <a href={"https://www.github.com/SaurabhTotey"}>GitHub</a>. I will showcase my favorite programming projects here.</p>
+		<h2>Projects</h2>
+		<p>Sorry, this section is a work in progress! My <a href={"https://www.github.com/SaurabhTotey"}>GitHub</a> has the majority of my projects. I will showcase my favorite programming projects in this section.</p>
+		<h2>Skills</h2>
+		<h4>Game Development</h4>
+		<p>TODO: talk about graphics</p>
+		<p>TODO: talk about audio</p>
+		<h4>Full-Stack</h4>
+		<p>TODO: talk about fullstack</p>
+		<h4>Simulations</h4>
+		<p>TODO: talk about simulations</p>
+		<h4>Scripting</h4>
+		<p>TODO: talk about scripting</p>
+		<h4>Scientific Software</h4>
+		<p>TODO: talk about scientific software</p>
+		<h4>Collaboration</h4>
+		<p>TODO: talk about collaboration</p>
+		<h4>Math</h4>
+		<p>TODO: talk about math</p>
+		<h2>Programming Languages</h2>
+		<p>I enjoy languages that have functional features, but I am also skilled in object-oriented programming. I often combine both styles of programming. I generally try to write declarative code whenever possible because I find well-written declarative code is also more readable.</p>
+		<p>TODO: talk about my favorite languages and why</p>
+		<p>TODO: enumerate all the languages I have worked with</p>
+		<h2>Setup</h2>
+		<h4>Version Control</h4>
+		<p>TODO: talk about git</p>
+		<h4>Editor</h4>
+		<p>TODO: talk about nvim, but also experience with JetBrains stuff</p>
+		<h4>Computer Setup</h4>
+		<p>TODO: talk about using linux, xmonad, polybar, etc.</p>
 	</Layout>
 )
