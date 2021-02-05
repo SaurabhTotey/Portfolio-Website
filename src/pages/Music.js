a8fb214b722bd41b27860415b7fc2cce0f10a24c
Saurabh Totey <SaurabhTotey@gmail.com>
2021-02-05 11:17:28 -0700

    Added piano recording

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Layout from "../components/Layout";
 
+// TODO: make a component that holds a piece's title, original artist, composer, and audio file
+
 const Music = () => (
 	<Layout>
 		<p>
-			Sorry, this page is a work in progress! I plan on uploading some recordings of me playing piano and possibly cello
+			Sorry, this page is a work in progress! I plan on uploading more recordings of me playing piano and possibly cello
 			here.
 		</p>
+		<h2>JUSTadICE</h2>
+		<p>
+			This song is by Seiko Oomori, but I am playing a piano composition by Fonzi M. The song appears as the seventh
+			opening of Black Clover.
+		</p>
+		<audio controls src="./audio/JUSTadICE.wav" />
 	</Layout>
 );
 export default Music;
